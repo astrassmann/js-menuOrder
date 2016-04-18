4852270192c5983e2a5769a1210f065279479d02
astrassmann <aidan.strassmann@wustl.edu>
2016-04-18 18:47:11 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,36 @@
 // begin with a blank/new order
-var order = '';
+var order = [];
 
+var entrees = ["None", "8 Chicken Nuggets","Chicken Sandwich","Chicken Salad", "Chicken Caesar Wrap" ];
+var sides  = ["None", "Regular Fries","Fruit Salad", "Chips" ];
+var drinks = ["None", "Chocolate Shake", "Strawberry Shake", "Vanilla Shake", "Soda", "Lemonade"];
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(foodArray) {
+
+  var orderPrompt = "Please type a number corresponding to your choice: \n";
+  var answerPrompt = "notanumber";
+
+  for(i=0;i<foodArray.length;i++){
+    orderPrompt += i + " - " + foodArray[i] + "\n";
+  }
+
+  while(isNaN(answerPrompt)){
+    answerPrompt = prompt(orderPrompt);
+  }
+
+ if(answerPrompt != 0){
+   order.push(foodArray[answerPrompt]);
+  }
 }
 
-// call the QUESTION function with the order options
-question();
+question(entrees);
+question(sides);
+question(drinks);
 
 // output their order
-document.write("<p>You ordered: " + order + "<p>");
+document.write("<h2>You order:"+"</h2>");
+for(j=0;j<order.length;j++){
+  document.write("<p>" +order[j] + "</p>");
+}
