f65d6a874b90243e48e11341c31776a9bdb2c76d
astrassmann <aidan.strassmann@wustl.edu>
2016-04-18 18:53:19 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,16 +4,17 @@ var order = [];
 var entrees = ["None", "8 Chicken Nuggets","Chicken Sandwich","Chicken Salad", "Chicken Caesar Wrap" ];
 var sides  = ["None", "Regular Fries","Fruit Salad", "Chips" ];
 var drinks = ["None", "Chocolate Shake", "Strawberry Shake", "Vanilla Shake", "Soda", "Lemonade"];
+var dessert = ["None", "Ice cream", "Chocolate chip cookie", "Brownie"]
 
 // build a function to ask questions
 
 function question(foodArray) {
 
-  var orderPrompt = "Please type a number corresponding to your choice: \n";
+  var orderPrompt = "Please type the number corresponding to your choice: \n";
   var answerPrompt = "notanumber";
 
   for(i=0;i<foodArray.length;i++){
-    orderPrompt += i + " - " + foodArray[i] + "\n";
+    orderPrompt += i + " ] " + foodArray[i] + "\n";
   }
 
   while(isNaN(answerPrompt)){
@@ -28,6 +29,7 @@ function question(foodArray) {
 question(entrees);
 question(sides);
 question(drinks);
+question(dessert);
 
 // output their order
 document.write("<h2>You order:"+"</h2>");
