1a8e954e7b227412f9f2faa2e5e49069ca267974
astrassmann <aidan.strassmann@wustl.edu>
2016-04-25 22:54:48 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,38 +1,28 @@
 // begin with a blank/new order
-var order = [];
+var numOrders = 0;
+var meal = [];
+var sauces = [];
+var i = 0;
+var j = 0;
 
-var entrees = ["None", "8 Chicken Nuggets","Chicken Sandwich","Chicken Salad", "Chicken Caesar Wrap" ];
-var sides  = ["None", "Regular Fries","Fruit Salad", "Chips" ];
-var drinks = ["None", "Chocolate Shake", "Strawberry Shake", "Vanilla Shake", "Soda", "Lemonade"];
-var dessert = ["None", "Ice cream", "Chocolate chip cookie", "Brownie"]
 
 // build a function to ask questions
 
-function question(foodArray) {
-
-  var orderPrompt = "Please type the number corresponding to your choice: \n";
-  var answerPrompt = "notanumber";
-
-  for(i=0;i<foodArray.length;i++){
-    orderPrompt += i + " ] " + foodArray[i] + "\n";
-  }
-
-  while(isNaN(answerPrompt)){
-    answerPrompt = prompt(orderPrompt);
-  }
-
- if(answerPrompt != 0){
-   order.push(foodArray[answerPrompt]);
-  }
+function question() {
+  // your code goes here
+    numOrders = prompt("How many entrees are you ordering?");
+    while (i < numOrders) {
+        meal[i] = prompt("Would you like nuggets or a chicken sandwich?");
+        sauces[i] = prompt("What kind(s) of sauces would you like on the side?");
+        i++;
+    }
 }
 
-question(entrees);
-question(sides);
-question(drinks);
-question(dessert);
+// call the QUESTION function with the order options
+question();
 
 // output their order
-document.write("<h2>You order:"+"</h2>");
-for(j=0;j<order.length;j++){
-  document.write("<p>" +order[j] + "</p>");
+while (j < numOrders) {
+    document.write("<p>You ordered a " + meal[j] + " with " + sauces[j] + " on the side. <p>");
+    j++;
 }
